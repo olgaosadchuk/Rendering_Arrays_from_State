5191e6f043a5d05c30603228fe7f7c6884ce8512
olgaosadchuk <osadchuk.olga.o@gmail.com>
2023-07-10 07:57:22 -0700

    R-ALAB 320H.3.1 - Rendering Arrays from State

---
+++ src/Learner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Score from './Score';
+
+function Learner({ learner }) {
+  return (
+    <div>
+      <h2>{learner.name}</h2>
+      <p>{learner.bio}</p>
+      <h3>Scores:</h3>
+      {learner.scores.map((score, index) => (
+        <Score key={index} score={score} />
+      ))}
+    </div>
+  );
+}
+
+export default Learner;
